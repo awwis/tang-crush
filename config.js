25c3d1a109ddcc39f32e3f5f047be6b555ce1805
awwis <88878787+awwis@users.noreply.github.com>
2021-08-13 09:06:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'anh bạn à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
+    nhưng Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
